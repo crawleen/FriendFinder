9b2255416d464bdae6a3cb18efc33633ab9cadb3
Miller <514094@bah.com>
2018-02-18 14:43:58 -0700

    server.js changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-require('./app/routing/apiRoutes.js')(app); 
-require('./app/routing/htmlRoutes.js')(app); 
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
   console.log('Friend Finder app is listening on PORT: ' + PORT);
