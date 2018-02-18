b339cc588ed3d43d866b28b174ac07848110545d
Miller <514094@bah.com>
2018-02-18 14:00:21 -0700

    2/18/18 additions

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsArray = [
+	{
+		name: 'Jan Brady',
+		photo: './images/Jan-Brady.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sponge Bob Square Pants',
+		photo: './images/spongeBob.png',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Felix the Cat',
+		photo: './images/felix.png',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]	
+	 }
+];
+
+module.exports = friendsArray;
